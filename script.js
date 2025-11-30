a0169645dba702d6980ea249e3e02210c9a09acf
Or Chen <orchen444@gmail.com>
2025-11-30 13:43:51 +0200

    opeartor + work

---
+++ script.js
@@ -0,0 +1,44 @@
+let x, y;
+let result;
+let operator;
+
+
+let display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll(".number");
+let plus = document.querySelector('#add');
+let subtract = document.querySelector('#subtract');
+let multiply = document.querySelector('#multiply');
+let divide = document.querySelector('#divide');
+let equal = document.querySelector('#equal');
+
+numberButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (x !== undefined) {
+            y = +btn.textContent
+            addNumberToDisplay(y)
+        }
+        else {
+            x = +btn.textContent
+            addNumberToDisplay(x)
+        }
+        console.log("x: " + x + "y: " + y)
+    });
+});
+
+plus.addEventListener('click', function () {
+    operator = '+'
+   addNumberToDisplay('+')
+})
+
+
+equal.addEventListener('click', function () {
+    if (operator == '+') {
+        //add the numbers
+        result = x + y;
+        display.textContent = result;
+    }
+})
+
+function addNumberToDisplay(num) {
+    display.textContent += num;
+}
